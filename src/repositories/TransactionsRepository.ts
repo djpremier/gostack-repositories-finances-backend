5275c8aea60008fd75e6209f2e96e50ab290cfb1
Diefferson Koderer Môro <contact@djpremier.dev>
2020-11-08 20:02:20 -0300

    🎉 Resolution

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const reducer = (
+      accumulator: Omit<Balance, 'total'>,
+      transaction: Transaction,
+    ) => {
+      switch (transaction.type) {
+        case 'income':
+          accumulator.income += transaction.value;
+          break;
+        case 'outcome':
+          accumulator.outcome += transaction.value;
+          break;
+        default:
+          break;
+      }
+
+      return accumulator;
+    };
+
+    const { income, outcome } = this.transactions.reduce(reducer, {
+      income: 0,
+      outcome: 0,
+    });
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
